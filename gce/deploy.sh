b866c2e7469e309bef582d540aec9c12feb54bba
Conor Samuel McFeely <cmcfeely03@qub.ac.uk>
2019-12-20 19:35:50 +0000

    minor changes

--- gce/deploy.sh
+++ gce/deploy.sh
@@ -1,20 +1,6 @@
-# Copyright 2019 Google LLC All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 set -ex
 
-# [START getting_started_gce_create_instance]
 MY_INSTANCE_NAME="my-app-instance-search"
 ZONE=us-central1-a
 
@@ -26,7 +12,6 @@ gcloud compute instances create $MY_INSTANCE_NAME \
     --metadata-from-file startup-script=startup-script.sh \
     --zone $ZONE \
     --tags http-server
-# [END getting_started_gce_create_instance]
 
 gcloud compute firewall-rules create default-allow-http-8080 \
     --allow tcp:8080 \
